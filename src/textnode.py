6c1471db4662a1988ea930007e4e762fe41dc4f1
gcid <gcidve@DESKTOP-7S5ORQG.>
2025-02-15 18:43:36 -0300

    start of TextNode(maybe finsh?

---
+++ src/textnode.py
@@ -0,0 +1,22 @@
+from enum import Enum
+
+class TextType(Enum):
+    NORMAL_TEXT = "normal"
+    BOLD_TEXT = "nold"
+    ITALIC_TEXT = "italic"
+    CODE_TEXT = "code"
+    LINK = "link"
+    IMAGE = "image"
+
+
+class TextNode:
+    def __init__(self, text, text_type, url=None):
+        self.text = text    # The text content of the node
+        self.text_type = text_type  #The type of text this node contains, which is a member of the TextType enum.
+        self.url = url  #The URL of the link or image, if the text is a link. Default to None if nothing is passed in.
+    
+    def __eq__(self, text_node):
+        return self.text == text_node.text and self.text_type == text_node.text_type and self.url == text_node.url
+    
+    def __repr__(self):
+        return f"TextNode({self.text}, {self.text_type.value}, {self.url})"
