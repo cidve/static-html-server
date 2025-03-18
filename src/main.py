e060daf0e00b73c783cacf0a57bea374e9090bda
cidve <gcidve@DESKTOP-7S5ORQG.>
2025-03-18 12:23:47 -0300

    Deploy web page

--- src/main.py
+++ src/main.py
@@ -13,7 +13,7 @@ dir_path_public = "./docs"
 markdown_folder_path = "./content/"
 template_path = "./template.html"
 html_folder_path = "./public/"
-default_basepath = "static-html-server/"
+default_basepath = "/"
 
 
 def main():
